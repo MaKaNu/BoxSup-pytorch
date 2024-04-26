8676bdfdb11dd7f228632b9b1853020d787a263b
MaKaNu <matti.kaupenjohann@fh-dortmund.de>
2024-04-26 17:11:00 +0200

    Update launch
    implements runner

--- boxsup_pytorch/launch.py
+++ boxsup_pytorch/launch.py
@@ -1,36 +1,27 @@
 """Main module."""
 import logging
-import logging.config
-import logging.handlers
-from pathlib import Path
 
 import torch
 from torch.backends import cudnn
-from torchvision import transforms
-import yaml
 
-logging_config_file = Path(__file__).parent.parent.resolve() / 'logging.conf'
-logging.config.fileConfig(logging_config_file)
-
-from config.config import GLOBAL_CONFIG
-from data.dataset import BoxSupDataset
-from model.network import FCN8s
-from pipeline.error_calc import ErrorCalc
-from pipeline.strats import GreedyStrat, MiniMaxStrat
-from pipeline.update_masks import UpdateMasks
-from pipeline.update_net import UpdateNetwork
+from boxsup_pytorch.config import GLOBAL_CONFIG
+from boxsup_pytorch.data.dataloader import BoxSupDataloader
+from boxsup_pytorch.model.network import FCN8s
+from boxsup_pytorch.pipeline.process_runner import ProcessRunner
+from boxsup_pytorch.pipeline.update_masks import UpdateMasks
+from boxsup_pytorch.pipeline.update_net import UpdateNetwork
 
 
 def main():
     """Call the main routine."""
     logger = logging.getLogger(__name__)
+    logger.debug("RUN: launch.py - main()")
 
     # load config data
-    nclass = GLOBAL_CONFIG.config().getany(section="DEFAULT", option="num_classes")
-    dataset_name = GLOBAL_CONFIG.config().get(section="DEFAULT", option="dataset")
-    num_iter = GLOBAL_CONFIG.config().getany(section="DEFAULT", option="num_iter")
+    nclass = GLOBAL_CONFIG.num_classes
 
-    data_dir = Path(__file__).parent.resolve() / f"data/datasets/{dataset_name}"
+    dataloader_mask = BoxSupDataloader("MASK")
+    dataloader_net = BoxSupDataloader("NET")
 
     # Specify the device the network is running on
     if torch.cuda.is_available():
@@ -39,45 +30,18 @@ def main():
     else:
         device = torch.device("cpu")
 
-    with open(data_dir / 'mean_std.yml', "r") as file:
-        statistic_data = yaml.safe_load(file)
-    mean = statistic_data['mean']
-    std = statistic_data['std']
-    transform = transforms.Compose([
-                transforms.Resize(256),
-                transforms.CenterCrop(224),
-                transforms.ToTensor(),
-                transforms.Normalize(mean, std)
-    ])
-
-    target_transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-    ])
-
-    dataset = BoxSupDataset(
-        data_dir / 'val',
-        transform,
-        target_transform
-    )
-
     # Prepare Network
     network = FCN8s(nclass=nclass, device=device).to(device)
 
     # Load Processes
-    error_calc = ErrorCalc(network)
-    greedy = GreedyStrat(error_calc)
-    minimax = MiniMaxStrat(error_calc)
-    update_masks = UpdateMasks(greedy, minimax, dataset)
-    update_net = UpdateNetwork(network)
+    process_runner = ProcessRunner(network=network)
 
-    logger.info("Begin Training")
-    for i in range(num_iter):
-        logger.info(f"Iteration {i} started")
-        update_masks.update()
-        images_and_masks = update_masks.get_outputs()
-        update_net.set_inputs(**images_and_masks)
-        update_net.update()
+    processes = [
+        UpdateMasks(network, dataloader_mask),
+        UpdateNetwork(network, dataloader_net),
+    ]
+
+    process_runner.run(processes)
 
 
 if __name__ == "__main__":
